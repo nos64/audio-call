b0e0a6df647bc6e6f97e0e052b0a86630a700759
nos64 <manosov1984@gmail.com>
2022-08-21 15:08:10 +0400

    feat: before refactor

---
+++ src/modules/api.ts
@@ -0,0 +1,16 @@
+import { AxiosResponse } from 'axios';
+import axiosInstance from './axiosInstance';
+import { Words } from './types';
+
+export const getWords = (group: number, page: number): Promise<AxiosResponse<Words[]>> => axiosInstance.get(`/words?group=${group}&page=${page}`);
+
+export const getWord = (id: string): Promise<AxiosResponse<Words>> => axiosInstance.get(`/words/${id}`);
+
+// eslint-disable-next-line consistent-return
+// export const getData = async (group: number, page: number) => {
+//   try {
+//     return await API.get(`/words?group=${group}&page=${page}`);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
---
+++ src/modules/renderPage.ts
@@ -0,0 +1,32 @@
+export const renderPage = () => `
+    <div class="wrapper">
+    <h1 class="audio__title">Аудиовызов</h1>
+    <div class="audio-wrapper">
+      <p class="audio__description">Тренировка Аудиовызов развивает словарный запас.</p>
+      <p class="audio__description">Вы должны выбрать перевод услышанного слова.</p>
+  
+      <div class="selection">
+        <h2 class="selection__title">Выберете уровень сложности</h2>
+        <ul class="selection__list">
+          <li class="selection__item">
+            <button class="selection__btn">1</button>
+          </li>
+          <li class="selection__item">
+            <button class="selection__btn">2</button>
+          </li>
+          <li class="selection__item">
+            <button class="selection__btn">3</button>
+          </li>
+          <li class="selection__item">
+            <button class="selection__btn">4</button>
+          </li>
+          <li class="selection__item">
+            <button class="selection__btn">5</button>
+          </li>
+          <li class="selection__item">
+            <button class="selection__btn">6</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  `;
