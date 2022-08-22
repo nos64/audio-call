03a550039337536b8a9613889b0fe720c9199bf5
nos64 <manosov1984@gmail.com>
2022-08-22 21:59:32 +0400

    feat: update backend

--- src/modules/axiosInstance.ts
+++ src/modules/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:27017',
+  // baseURL: 'http://localhost:27017',
+  baseURL: 'https://rs-react-learnwords.herokuapp.com',
 });
